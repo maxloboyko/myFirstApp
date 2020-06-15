4cbe3bef4f2a92080fecdc1b1f6e822ba6b8f87d
Max <loboykomax92@gmail.com>
2020-06-15 23:20:32 +0300

    первое задание по приложению

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+const personalMoviDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMoviDB.movies[a] = b;
+personalMoviDB.movies[c] = d;
+
+console.log(personalMoviDB);
